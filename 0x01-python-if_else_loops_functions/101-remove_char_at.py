504b41e0b09944102725b575f82fa5bb04a6741b
Arnold Henry <devharnold@gmail.com>
2023-08-10 12:17:27 +0000

    make-changes

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def remove_char_cat(string, n):
-	if n >= 0:
-		return string[:n] + string[n + 1:]
-	else:
-		return string
+     if n >= 0:
+	return string[:n] + string[n + 1:]
+     else:
+	return string
