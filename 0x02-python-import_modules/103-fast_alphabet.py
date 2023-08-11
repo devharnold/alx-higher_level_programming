cf931d5334db032b89673dc965b757aa6d04edd9
Arnold Henry <devharnold@gmail.com>
2023-08-11 14:52:02 -0700

    change

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
-
 import string
-
-def print_uppercase_ascii():
-    """PPrint Uppercase letters."""
-    print(string.ascii_uppercase)
-
-print_uppercase_ascii()
+print(''.join(getattr(string, 'ascii_uppercase')))
