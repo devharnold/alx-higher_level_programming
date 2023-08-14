a4631259f6c0b8aa6b141a41b36b3bb477bd8033
Arnold Henry <devharnold@gmail.com>
2023-08-14 13:25:40 -0700

    change

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def no_c(my_string):
+    new_string = my_string.translate({ord('c'): None})
+    new_string = new_string.translate({ord('C'): None})
+    return new_string
+print (new_string)
