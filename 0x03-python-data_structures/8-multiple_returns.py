87752d5cd6c6fe59dabe31720d10b669679c7d78
Arnold Henry <devharnold@gmail.com>
2023-08-14 13:44:34 -0700

    mul-ret

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def multiple_returns(sentence):
+    if len(sentence) == 0:
+        first_char = None
+    else:
+        first_char = sentence[0]
+        return (len(sentence), first_char)
+
+input_sentence = "Holberton, School"
+result = multiple_returns(input_sentence)
+print(result)
