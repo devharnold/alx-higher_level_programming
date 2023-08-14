a308e8e0c3e5f611a91c6c769571da91a4a86146
Arnold Henry <devharnold@gmail.com>
2023-08-14 13:33:29 -0700

    change

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
 
 def print_max_integer(matrix=[[]]):
-    for num in row:
-        for i in matrix:
-            print("{:d}".format(num), end='\t')
-        print()
-    print_max_integer(matrix)
-    print('--')
-    print_matrix_integer()
+    for i in matrix:
+        print(" ".join("{:d}".format(j) for j in i))
