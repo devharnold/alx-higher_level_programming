069386e00a6657e6249278cf3356f12e7bd78e90
Arnold Henry <devharnold@gmail.com>
2023-08-29 10:17:26 +0300

    last-task

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -1,22 +1,11 @@
-#include "/usr/include/python3.4/Python.h"
-#include <stdio.h>
-#include <stdlib.h>
+#include <Python.h>
 
-void print_hexn(const char *str, int n);
+
 void print_python_float(PyObject *p);
 void print_python_bytes(PyObject *p);
 void print_python_list(PyObject *p);
 
-void print_hexn(const char *str, int n)
-{
-	int i = 0;
 
-	for (; i < n - 1; ++i)
-		printf("%02x ", (unsigned char) str[i]);
-
-	printf("%02x", str[i]);
-    fflush(stdout);
-}
 
 void print_python_bytes(PyObject *p)
 {
