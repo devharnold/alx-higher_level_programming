9cec8befc70bf28b6e73a3b29f689ba7fcc1053e
Arnold Henry <devharnold@gmail.com>
2023-08-28 22:18:58 -0700

    divides ele by ele 2 lists

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -3,16 +3,20 @@ def list_division(my_list_1, my_list_2, list_length):
     new_list = []
     for i in range(list_length):
         try:
-            result = my_list_1[i] / my_list_2[i]
-        except(ValueError, TypeError):
-            print("wrong type")
-            result = 0
+            new_list.append(my_list_1[i] / my_list_2[i])
         except ZeroDivisionError:
-            print("division by 0")
-            result = 0
+            new_list.append(0)
+            print('division by 0')
+            continue
         except IndexError:
-            print("out of range")
-            result = 0
+            new_list.append(0)
+            print('out of range')
+            continue
+        except TypeError:
+            new_list.append(0)
+            print('wrong type')
+            continue
         finally:
-            new_list.append(result)
-    return new_list
+            pass
+        return new_list
+
