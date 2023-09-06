534db0a1a6c6c6fa96afe2fc0ae035eb9c8e59d8
Arnold Henry <devharnold@gmail.com>
2023-09-06 12:04:27 +0300

    refactor

--- 0x08-python-more-classes/0-rectangle.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""
-An empty class rectangle
-"""
-
-class Rectangle:
-    """Empty representation of rectangle"""
-    pass
--- 0x08-python-more-classes/README.md
+++
@@ -1,5 +0,0 @@
-This is work done to do the task including more classes in python
-
-New concepts introduced :  __str__,  __repr__, 
-
-
