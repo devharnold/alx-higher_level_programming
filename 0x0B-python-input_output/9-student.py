6e84d52efeeeecb2caa0c077ab13b41286cd5e53
Arnold Henry <devharnold@gmail.com>
2023-09-13 00:26:05 +0300

    studen

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Class "Student"
+"""
+
+class Student:
+    """student"""
+    def __init__(self, first_name, last_name, age):
+        """Initializes the student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """returns a dictionary representation of the instance student"""
+        return self.__dict__
