ff16c09005b9465e44060e55e4ae8b510b760897
Arnold Henry <devharnold@gmail.com>
2023-09-13 20:42:08 +0300

    refactor

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -7,11 +7,14 @@ Rectangle = __import__('9-rectanlge').Rectangle
 class Square(Rectangle):
     """created the class inititalize the objects and work out"""
     def __init__(self, size):
-        """instance of the square"""
+        """instance of the square"
+        use of self.integer_validator
+        """
         self.integer_validator("size", size)
         self.__size = size
         super().__init__(size, size)
 
     def area(self):
-        """area of the square"""
+        """area of the square
+        return the private instance ** 2"""
         return self.__size ** 2
--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
+
+"""class MyInt"""
 class MyInt(int):
+    """definition of new class items used, args, kwargs"""
     def __new__(cls, *args, **kwargs):
+        """returns super(result of top)"""
         return super(MyInt, cls).__new__(cls, *args, **kwargs)
     
     def __eq__(self, other):
+        """definition of __eq__ function"""
         """!= -> =="""
         return int(self) != other
     
