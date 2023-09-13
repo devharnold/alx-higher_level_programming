69b8d8768af59ecbeb83c1ec6def96551a3fe08f
Arnold Henry <devharnold@gmail.com>
2023-09-13 20:11:51 +0300

    refactor

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,14 +1,22 @@
 #!/usr/bin/python3
+
+"""import Rectangle from 9-rectangle"""
 Rectangle = __import__('9-rectangle').Rectangle
 
+"""class Square"""
 class Square(Rectangle):
+    """initialize the objects of the class rectangle
+    use different functions such the magic methods
+    """
     def __init__(self, size):
         self.integer_validator("size", size)
         self.__size = size
         super().__init__(size, size)
 
     def area(self):
+        """define the area() function"""
         return self.__size ** 2
 
     def __str__(self):
+        """the magic method __str__"""
         return "[Square] {:d}/{:d}".format(self.__size, self.__size)
