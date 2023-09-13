0b372e00cbce38df21f880e1c26cb177cb1c5427
Arnold Henry <devharnold@gmail.com>
2023-09-13 20:07:58 +0300

    refactor

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
-"""Superclass BaseGeometry Subclass Rectangle"""
+"""Superclass BaseGeometry Subclass Rectangle and import"""
 Rectangle = __import__('9-rectanlge').Rectangle
 
+"""class Square"""
 class Square(Rectangle):
+    """created the class inititalize the objects and work out"""
     def __init__(self, size):
         """instance of the square"""
         self.integer_validator("size", size)
