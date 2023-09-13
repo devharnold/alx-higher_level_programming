b43f8116a0c578b49c6f4ac211563b14b7f27820
Arnold Henry <devharnold@gmail.com>
2023-09-13 19:58:58 +0300

    refactor

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,10 +1,22 @@
 #!/usr/bin/python3
+
+"""Import BaseGeometry from file 7-base_geometry"""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+"""
+class Rectangle
+"""
+
 class Rectangle(BaseGeometry):
+    """class rectangle created"""
     def __init__(self, width, height):
+        """definition of class rectangle
+        together with self.integer_validator
+        """
         self.integer_validator("width", width)
         self.integer_validator("height", height)
+
+        """private instances"""
 
         self.__width = width
         self.__height = height
