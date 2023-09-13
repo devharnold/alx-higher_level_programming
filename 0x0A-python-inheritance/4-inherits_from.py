336dea272b56319db52d21f4a9d7eceab6484abb
Arnold Henry <devharnold@gmail.com>
2023-09-13 19:44:13 +0300

    refactor

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+
+"""inherits_from -> function definition"""
+
 def inherits_from(obj, a_class):
+    """Returns true if obj is a subclass of a_class, else false"""
     if isinstance(obj, a_class) and \
        issubclass(a_class, obj.__class__) is False:
         return True
