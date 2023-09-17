ad9112b139db6dd08630e3a1c4ceb59d74c4fbc5
Arnold Henry <devharnold@gmail.com>
2023-09-17 18:30:36 +0300

    create

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,3 +3,25 @@
 import json
 import csv
 import turtle
+
+
+class Base:
+    """Represent base model. The base will act as 'base' 
+    for all other classes.
+    Attributes:
+    __nb_objects (int): Number of instantiated bases.
+    """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Initialize a fresh/new base
+        Args:
+        id (int) : id of the new base.
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+            
