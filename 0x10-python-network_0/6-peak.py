801faab10e034d797a42d380ad313be17e64479a
Arnold Henry <devharnold@gmail.com>
2023-12-03 09:20:00 +0300

    overdue

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""finding peak number in list of integers"""
+def find_peak(list_of_integers):
+    list_of_integers = [5, 6, 7, 9, 8]
+    for num in list_of_integers:
+        if num >= :
+            result = (find_peak(list_of_integers=[]))
+            print (result)
+
+
+if __name__ == "__main__":
+    list_of_numbers = [1, 4, 5, 9, 6, 7]
+    print(find_peak(list_of_integers=[1, 4, 5, 9, 6, 7]))
