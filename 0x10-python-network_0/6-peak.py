b8fc6526219bc91fad8c7fea25cf6ccb67ee330a
Arnold Henry <devharnold@gmail.com>
2023-12-03 11:57:27 +0300

    peak_number

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python3
 
-"""finding peak number in list of integers"""
+"""Description of the find peak"""
 def find_peak(list_of_integers):
-    list_of_integers = [5, 6, 7, 9, 8]
-    for num in list_of_integers:
-        if num >= :
-            result = (find_peak(list_of_integers=[]))
-            print (result)
+    length = len(list_of_integers)
+    m = length // 2
+    li = list_of_integers
+
+    if m - 1 < 0 and m + 1 >= length:
+        return li[m]
+    elif m - 1 < 0:
+        return li[m] if li[m] > li[m + 1] else li[m + 1]
+    elif m + 1 >= length:
+        return li[m] if li[m] > li[m - 1] else li[m - 1]
+
+    if li[m - 1] < li[m] > li[m + 1]:
+        return li[m]
+
+    if li[m + 1] > li[m - 1]:
+        return find_peak(li[m:])
+    return find_peak(li[:m])
 
 
-if __name__ == "__main__":
-    list_of_numbers = [1, 4, 5, 9, 6, 7]
-    print(find_peak(list_of_integers=[1, 4, 5, 9, 6, 7]))
